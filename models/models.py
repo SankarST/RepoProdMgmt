513cdc1be38c5f7034f8de3283a2e88b7557b6c8
sankar <sankar.sthillai@gmail.com>
2021-04-28 22:35:41 +0530

    Check unique for default_code when copy

--- models/models.py
+++ models/models.py
@@ -1,20 +1,38 @@
 #-*- coding: utf-8 -*-
+import logging
 
-from odoo import models, fields, api
+from odoo import models, fields, api, exceptions
 
+logger = logging.getLogger(__name__)
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-#    _name = 'prod_mgmt.prod_mgmt'
-#    _description = 'prod_mgmt.prod_mgmt'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+#    _sql_constraints = [
+#        ('sku_unique',
+#         'UNIQUE(default_code)',
+#         "This SKU already exists for another product. Please enter a new (non-existing) SKU"),
+#    ]
+
+
+
+    
+    @api.constrains('default_code')
+    def _check_sku_unique(self):
+        
+        logging.info("--- Prod Mgmt --- : Entering sku check ")
+        for rec in self:
+            if rec.default_code:
+                logging.info("--- Prod Mgmt --- : SKU is %s for  product %s " % (str(rec.default_code) , str(rec.id)))
+                prod_template = self.env['product.template'].search([
+                ('default_code', '=', rec.default_code),('id','!=',rec.id)
+                ],limit=1)
+
+                if prod_template:
+                    logging.info("--- Prod Mgmt --- : SKU exists for Prod template : %s " %str(prod_template.id))
+                    raise exceptions.ValidationError("SKU [ %s ] already exists for another product. Please enter a new (non-existing) SKU" %(rec.default_code ))
+                else:
+                    logging.info("--- Prod Mgmt --- : SKU : Prod template not found")
+            else:
+                logging.info("--- Prod Mgmt --- : SKU : Not found " )
